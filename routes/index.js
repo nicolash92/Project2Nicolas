b3bd68eee1bbab0981e5ed9be48ec9bb2e78edfb
nicolash92 <nicolas.hanout@gmail.com>
2017-11-14 09:54:30 -0800

    commit

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,11 @@ var express = require('express');
 var router = express.Router();
 var controllerDB = require('../controllers/database');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-router.get('/getAllOrders', controllerDB.getAllRoutes());
+router.get('/getAllOrders', controllerDB.getAllOrders);
 /*
 router.post("/storeData", controllerDB.storeData);
 */
