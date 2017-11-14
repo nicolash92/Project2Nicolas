2c56659095472cb916f36c49de4edefe764b3a4b
nicolash92 <nicolas.hanout@gmail.com>
2017-11-14 09:36:28 -0800

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
-var mongoDBURI = process.env.MONGODB_URI || 'mongodb://nicolas:nicpass@ds255265.mlab.com:55265/heroku_3lrv21zs';
 var controllerDB = require('../controllers/database');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
-router.post("/storeData", controllerDB.storeData());
 
-
+router.get('/getAllOrders', controllerDB.getAllRoutes());
+/*
+router.post("/storeData", controllerDB.storeData);
+*/
 module.exports = router;
