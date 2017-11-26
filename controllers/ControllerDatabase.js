7c953925c1d810fcd2af88b386d2c39c8344ffa6
nicolash92 <nicolas.hanout@gmail.com>
2017-11-26 13:22:15 -0800

    commit

--- controllers/ControllerDatabase.js
+++ controllers/ControllerDatabase.js
@@ -84,8 +84,8 @@ module.exports.storeData =  function (request, response) {
             BILLING_ID: billingID,
             SHIPPING_ID: shippingID,
             //DATE: '',//new Date(),
-            //PRODUCT_VECTOR: '',//cart.items,
-            //ORDER_TOTAL: cart.totalPrice
+            PRODUCT_VECTOR: '',//cart.items,
+            ORDER_TOTAL: cart.totalPrice
         };
 
         Orders.insertOne(ordersData, function (err, docs) {
